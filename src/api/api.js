529349d3c1e80632e135c92ec77789bd1088d391
vishalgoudar7 <133781628+vishalgoudar7@users.noreply.github.com>
2025-07-30 15:34:35 +0530

    upadate

--- src/api/api.js
+++ src/api/api.js
@@ -12,7 +12,7 @@ const API_CONFIG = {
       presetToken: "e52a308c58887782d13a6fce7ae0258f8b6dfde1",
     },
   },
-  current: 'beta',
+  current: 'live',
 };
 
 const { base, presetToken } = API_CONFIG.servers[API_CONFIG.current];
